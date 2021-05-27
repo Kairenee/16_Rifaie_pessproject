b0fd83dd4e784f4aa2816049cbd27a4f08c03bb9
Kairenee <84893199+Kairenee@users.noreply.github.com>
2021-05-27 13:52:10 +0800

    initial commit

---
+++ nav.php
@@ -0,0 +1,9 @@
+<h1>Police Emergency Service System</h1>
+<nav id="HeaderStyle">
+  <ul>
+	<li><a href="logcall.php"><span>Log Call</a></span></li>
+	<li><a href="Update.php"><span>Update</a></span></li>
+	<li><a href="#"><span>Report</a></span></li>
+	<li><a href="#"><span>History</a></span></li>
+	</ul>
+</nav>
